d07f64a0c63de88063d2c63a12ab11446fc036bf
= <=>
2023-05-04 21:05:24 -0700

    EVENLISTENER

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+    const buttonElement = document.getElementById('button');
+  
+    if (buttonElement) {
+      buttonElement.addEventListener('click', function() {
+        console.log('Button clicked!');
+      });
+    } else {
+      console.error('Element with id "button" not found');
+    }
+  }
